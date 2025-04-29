86c38585805cf96f14fb8f5233acdafce92dc444
Annanayan <yanwang5@andrew.cmu.edu>
2025-04-29 02:34:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 const buttons = document.querySelectorAll('.nav-btn');
 const contents = document.querySelectorAll('.content');
 
+// 页面加载时，默认显示 MainPage
+document.addEventListener('DOMContentLoaded', () => {
+    contents.forEach(content => content.classList.add('hidden'));
+    document.getElementById('MainPage').classList.remove('hidden');
+});
+
+
 // 为每个按钮添加点击事件
 buttons.forEach(button => {
     button.addEventListener('click', () => {
